90d6acd03808ea3cc75d60be30e1975021e7cbdc
Yohann <yohann.antunes18@gmail.com>
2024-08-02 13:48:08 -0500

    event add and listener add

---
+++ app/Listeners/RedirectAfterUserLogin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserLoggedIn;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class RedirectAfterUserLogin
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(UserLoggedIn $event): void
+    {
+        //
+    }
+}
